ef4c8ba123bbb79237ea2845540811abbe240158
Diego Fernandes <32878177+diego-s-fernandes@users.noreply.github.com>
2022-11-08 16:27:32 -0300

    iteracao 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import IdCard from './components/IdCard/IdCard.jsx';
+const IdCards = [
+  {
+    lastName: 'Doe',
+    firstName: 'John',
+    gender: 'male',
+    height: 178,
+    birth: new Date('1992-07-14').toDateString(),
+    picture: 'https://randomuser.me/api/portraits/men/55.jpg',
+  },
+  {
+    lastName: 'Delores',
+    firstName: 'Obrien',
+    gender: 'female',
+    height: 172,
+    birth: new Date('1988-05-11').toDateString(),
+    picture: 'https://randomuser.me/api/portraits/women/55.jpg',
+  },
+];
 function App() {
+  
+ 
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {/* ITERATION 1 */}
+       <div className="Cards-container">
+        <h2>IdCards</h2>
+        {IdCards.map(
+          ({ lastName, firstName, gender, height, birth, picture }) => {
+            return (
+              <IdCard
+                lastName={lastName}
+                firstName={firstName}
+                gender={gender}
+                height={height}
+                birth={birth}
+                picture={picture}
+                key={picture}
+              />
+            );
+          }
+        )}
+       </div>
+     
+  </div>
   );
 }
 
